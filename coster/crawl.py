1068cd66f44b8ec757edabe28ed21c0a1ebb864b
sksdk34 <gim34@naver.com>
2019-09-06 18:41:52 +0900

    Update Crwaling Function

--- coster/crawl.py
+++ coster/crawl.py
@@ -8,59 +8,49 @@ django.setup()
 
 #평수
 def space():
-    req = requests.get('https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&mapLevel=10')
+    req = requests.get('https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2400&ptpNo=&bssYm=&schlCd=&cmplYn=&page=1#_content_list_target')
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
 
     data = []
-    num = len(soup.select('.num > .inner .calc_area'))
+    num = len(soup.select('.rate_layer > .layer > .calc_area'))
     for i in range(0, num - 1):
-        items = soup.select('.num > .inner .calc_area')[i].text
-        result = ''
-        for test in items:
-            result += test
-
-        result = result.split() #공백 제거
+        items = soup.select('.rate_layer > .layer > .calc_area')[i].text.split()
+        text = items[1]
+        result = text[:-1]
         data.append(result)
 
     return data
 
 #층수
 def floor():
-    req = requests.get(
-        'https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&mapLevel=10')
+    req = requests.get('https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2400&ptpNo=&bssYm=&schlCd=&cmplYn=&page=1#_content_list_target')
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
 
     data = []
     num = len(soup.select('.num2 > .inner > span'))
     for i in range(0, num - 1):
-        items = soup.select('.num2 > .inner > span')[i].text
-        result = ''
-        for test in items:
-            result += test
-
-        result = result.split() #공백 제거
+        items = soup.select('.num2 > .inner > span')[i].text.split()
+        text = items[0]
+        result = text[:-3]
         data.append(result)
 
     return data
 
 #가격
 def price():
-    req = requests.get(
-        'https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&mapLevel=10')
+    req = requests.get('https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2400&ptpNo=&bssYm=&schlCd=&cmplYn=&page=1#_content_list_target')
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
 
     data = []
     num = len(soup.select('.num > .inner > strong'))
     for i in range(0, num - 1):
-        items = soup.select('.num > .inner > strong')[i].text
-        result = ''
-        for test in items:
-            result += test
-
-        result = result.split() #공백 제거
+        items = soup.select('.num > .inner > strong')[i].text.split()
+        text = items[0]
+        result = text[:-4]
+        result += '000'
         data.append(result)
 
     return data
