a0c61d28f5a080589710c8823c0403f6c71764a9
macarize <54901021+macarize@users.noreply.github.com>
2019-09-10 16:35:13 +0900

    false directory

--- MachineLearningMF/AddBias.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-class AddBias:
-    def addB(self, data):
-        ones = np.ones((data.m, 1), dtype=int)
-        data.x = np.concatenate((ones, data.x), axis=1)
-        return data
--- MachineLearningMF/Loadcvs.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-
-class Loadcvs:
-    def Loadcvs(self, path):
-        matrix = np.loadtxt(path, delimiter=",", dtype=np.float32)
-        return matrix
