2572bf6e49ec468ebaba9cf8805efc368f411596
MinSeop Kim <gim34@naver.com>
2019-09-15 19:13:50 +0900

    Fix Crawl.py

--- coster/crawl.py
+++ coster/crawl.py
@@ -6,14 +6,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coster.settings")
 django.setup()'''
 # from .app.models import crawl
 import random
-#평수
-def space():
-    data = []
-    for page in range(1, 10):
-        link = "https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2400&ptpNo=&bssYm=&schlCd=&cmplYn=&page={0}#_content_list_target".format(page)
-        req = requests.get(link)
-        html = req.text
-        soup = BeautifulSoup(html, 'html.parser')
 
 class crawl:
     #평수
@@ -37,62 +29,44 @@ class crawl:
         return data
 
     #층수
-    def floor(self):
+    def floor():
         data = []
-        for page in range(1, 2):
-            print('page%d' % page)
+        for page in range(1, 10):
             link = "https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2400&ptpNo=&bssYm=&schlCd=&cmplYn=&page={0}#_content_list_target".format(page)
             req = requests.get(link)
             html = req.text
             soup = BeautifulSoup(html, 'html.parser')
 
-#층수
-def floor():
-    data = []
-    for page in range(1, 10):
-        link = "https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2400&ptpNo=&bssYm=&schlCd=&cmplYn=&page={0}#_content_list_target".format(page)
-        req = requests.get(link)
-        html = req.text
-        soup = BeautifulSoup(html, 'html.parser')
-
-        num = len(soup.select('.num2 > .inner > span'))
-        for i in range(0, num - 1):
-            items = soup.select('.num2 > .inner > span')[i].text.split()
-            text = items[0]
-            if(len(text) != 3):
-                result = text[:-3]
-                if result == '저':
-                    data.append(random.randrange(1,11))
-                elif result == '중':
-                    data.append(random.randrange(11,21))
-                elif result == '고':
-                    data.append(random.randrange(21,31))
+            num = len(soup.select('.num2 > .inner > span'))
+            for i in range(0, num - 1):
+                items = soup.select('.num2 > .inner > span')[i].text.split()
+                text = items[0]
+                if(len(text) != 3):
+                    result = text[:-3]
+                    if result == '저':
+                        data.append(random.randrange(1,11))
+                    elif result == '중':
+                        data.append(random.randrange(11,21))
+                    elif result == '고':
+                        data.append(random.randrange(21,31))
+                    else:
+                        data.append(result)
                 else:
-                    data.append(result)
-            else:
-                result = text[0:-2]
-                if result == '저':
-                    data.append(random.randrange(1,11))
-                elif result == '중':
-                    data.append(random.randrange(11,21))
-                elif result == '고':
-                    data.append(random.randrange(21,31))
-                else:
-                    data.append(result)
-    return data
-#가격
-def price():
-    data = []
-    for page in range(1, 10):
-        link = "https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2400&ptpNo=&bssYm=&schlCd=&cmplYn=&page={0}#_content_list_target".format(page)
-        req = requests.get(link)
-        html = req.text
-        soup = BeautifulSoup(html, 'html.parser')
+                    result = text[0:-2]
+                    if result == '저':
+                        data.append(random.randrange(1,11))
+                    elif result == '중':
+                        data.append(random.randrange(11,21))
+                    elif result == '고':
+                        data.append(random.randrange(21,31))
+                    else:
+                        data.append(result)
+        return data
 
     #가격
     def price(self):
         data = []
-        for page in range(1, 2):
+        for page in range(1, 10):
             print('page%d' % page)
             link = "https://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1135010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2400&ptpNo=&bssYm=&schlCd=&cmplYn=&page={0}#_content_list_target".format(page)
             req = requests.get(link)
@@ -109,15 +83,7 @@ def price():
                 data.append(result)
 
         return data
-
-if __name__ == '__main__':
-    data1 = space()
-    print(len(data1))
-    data2 = floor()
-    print(len(data2))
-    data3 = price()
-    print(len(data3))
-    
+   
     def crawlRun(self):
         qubic = self.space()
         floor = self.floor()
