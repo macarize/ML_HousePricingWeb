2e3b1cba2584cdbfc39b4cff6502ae9b4ee5da8b
macarize <54901021+macarize@users.noreply.github.com>
2019-09-17 21:56:34 +0900

    added features

--- coster/MachineLearningMF/Converter.py
+++ coster/MachineLearningMF/Converter.py
@@ -1,34 +1,81 @@
-import sys
 import numpy as np
+<<<<<<< Updated upstream
 sys.path.insert(0, '..\MachineLearningMF\crawl.py')
 import crawl
 import re
+=======
+import pandas as pd
+import testCrawl
+>>>>>>> Stashed changes
+
 
 class DataLoad:
-    '''cl = crawl.crawl()'''
-
-    qubic = []
+    '''remove followed annotation when you want to crawl data again'''
+    '''cl = testCrawl.testCrawl()'''
+    space = []
+    '''annotated because we will treat this by saving files seperatly'''
+    '''kind = []'''
     floor = []
+    rooms = []
+    year = []
     price = []
-    ''''[qubic, floor, price] = cl.crawlRun()'''
 
-    qubic = np.array(qubic)
+    #[space, floor, rooms, year, price] = cl.testCrawlRun()
+
+    space = np.array(space)
+    '''annotated because we will treat this by saving files seperatly'''
+    '''kind = np.array(kind)'''
     floor = np.array(floor)
+    rooms = np.array(rooms)
+    year = np.array(year)
     price = np.array(price)
 
-    qubic = qubic.reshape(qubic.size, 1)
-    floor = floor.reshape(floor.size, 1)
-    price = price.reshape(price.size, 1)
+    n = price.size
+
+    space = space.reshape(n, 1)
+    '''annotated because we will treat this by saving files seperatly'''
+    '''''''#kind = kind.reshape(n, 1)'''
+    floor = floor.reshape(n, 1)
+    rooms = rooms.reshape(n, 1)
+    year = year.reshape(n, 1)
+    price = price.reshape(n, 1)
+
 
     def Dataget(self):
-        return [self.qubic, self.floor, self.price]
+        return [self.space, self.floor,
+                self.rooms, self.year, self.price]
 
     def ConvertToCSV(self):
+        self.price = np.concatenate((self.space, self.price), axis=1)
+        '''annotated because we will treat this by saving files seperatly'''
+        '''elf.price = np.concatenate((self.kind, self.price), axis=1)'''
         self.price = np.concatenate((self.floor, self.price), axis=1)
-        self.price = np.concatenate((self.qubic, self.price), axis=1)
+        self.price = np.concatenate((self.rooms, self.price), axis=1)
+        self.price = np.concatenate((self.year, self.price), axis=1)
         DataCSV = self.price
-        np.savetxt("Data.txt", DataCSV, delimiter=",",fmt="%s")
+        '''annotated because there was conflicting encoding problems'''
+        '''np.savetxt("DataNew.txt", DataCSV.encode('utf8'), delimiter=",", fmt="%s")'''
+        pd.DataFrame(DataCSV).to_csv("DataNew.csv", index=False, header=False)
 
 
+    '''annotated becaouse there were enough testing'''
+    '''# 가격 결과 출력
+    print("price : ", price)
+    print("price(len) : ", len(price))
+    # 공급면적 결과 출력
+    print("space : ", space)
+    print("space(len) : ", len(space))
+    # 매물종류 결과 출력
+    #print("kind : ", kind)
+    #print("kind(len) : ", len(kind))
+    # 층수 결과 출력
+    print("floor : ", floor)
+    print("floor(len) : ", len(floor))
+    # 방수 결과 출력
+    print("rooms : ", rooms)
+    print("rooms(len) : ", len(rooms))
+    # 연식 결과 출력
+    print("year : ", year)
+    print("year(len) : ", len(year))'''
 
 
--- coster/MachineLearningMF/Main.py
+++ coster/MachineLearningMF/Main.py
@@ -1,5 +1,6 @@
 from MachineLearningMF import DataInit, AddBias, GradientDescent, Normalization, Converter, runMachine
 
+<<<<<<< Updated upstream
 import numpy as np
 
 
@@ -40,4 +41,54 @@ print(string)
 print(theta)
 # cl = Converter.DataLoad()
 # cl.ConvertToCSV()
+=======
+import os.path
+import numpy as np
+
+
+def ml(year,rooms,floor,space):
+    BASE = os.path.dirname(os.path.abspath(__file__))
+
+    print(os.path.join(BASE, "DataNew.csv"))
+    Path = os.path.join(BASE, "DataNew.csv")
+    # Path = "..\MachineLearningMF\Data.txt"
+
+    '''somethin somthin'''
+    data = DataInit.DataInit()
+    CostFuntion = runMachine.runMachine()
+    GDescent = GradientDescent.GradientDescent()
+    Norm = Normalization.Normalization()
+    AddBias1 = AddBias.AddBias()
+
+    '''loac cvs'''
+    data.loader(Path)
+    '''theta init'''
+    '''initiated optimal theta 17/9/2019'''
+    theta = np.array([[43000.01475504], [2504.21369816], [5321.04358409], [-613.123852], [4505.00713087]])
+    '''Normalize'''
+    data.x, mu, sigma = Norm.featureNormalize(data)
+    '''Add Bias Column'''
+    data = AddBias1.addB(data)
+    '''remove annotations when you compute theta again'''
+    '''run Gradient descent and Cost funtion'''
+    '''theta = GDescent.runGradient(data,theta,0.001,100000)
+    theta = theta.reshape(1, 5)'''
+
+    '''compute housing price'''
+    r = np.array([year,rooms,floor,space])
+    r = (r - mu)/sigma
+    r2 = np.ones(r.shape[0]+1)
+    r2[1:] = r
+    r = r2
+    price = np.dot(r.reshape(1,5), theta.reshape(5,1))
+    print('\nPredicted price of a 1650 sq-ft, 3 br house (using gradient descent): ', price[0][0])
+    return price[0][0]
+
+ml(2001,3,1,77.68)
+
+
+#cl = Converter.DataLoad()
+#cl.ConvertToCSV()
+
+>>>>>>> Stashed changes
 
