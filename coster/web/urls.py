6b5927b8b0812dedd00b85889da2d91f0b184736
sksdk34 <gim34@naver.com>
2019-09-22 20:43:24 +0900

    move files to the static folder

--- coster/web/urls.py
+++ coster/web/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('signup', views.signup, name='signup'),
-    # path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('consultant', views.consultant, name='consultant'),
     path('login', views.login, name='login'),
     path('find', views.find, name='find'),
+    path('Hub', views.Hub, name='Hub'),
+    path('Intro', views.Intro, name='Intro'),
+    path('Page2', views.Page2, name='Page2'),
 ]
