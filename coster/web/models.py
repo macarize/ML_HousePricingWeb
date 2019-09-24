dcdabd66c998b8780c2202a38684bb12223d8c3b
sksdk34 <gim34@naver.com>
2019-09-24 14:46:52 +0900

    Add sigudong ajax function

--- coster/web/models.py
+++ coster/web/models.py
@@ -6,3 +6,9 @@ class user(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     power = models.IntegerField()
+
+class consulting(models.Model):
+    si = models.CharField(max_length=100)
+    gu = models.CharField(max_length=100)
+    dong = models.CharField(max_length=100, primary_key=True)
+    theta = models.FloatField()
