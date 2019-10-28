9dbc537206ee43756a67ae4bb4f9def1a7f66dc0
sksdk34 <gim34@naver.com>
2019-10-28 15:34:02 +0900

    fixed untracked files

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\leehy\Documents\GitHub\ML_HousePricingWeb\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
